1a12dd50bef3f61456b0ba5e84d8e84f92ae286b
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-07-14 02:26:14 -0400

    basic infomation and testing channel

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const p = document.getElementById("player");
+if (Hls.isSupported()) {
+	var hls = new Hls();
+	hls.loadSource("https://weather-lh.akamaihd.net/i/twc_1@92006/master.m3u8");
+	hls.attachMedia(p);
+	hls.on(Hls.Events.MANIFEST_PARSED,function(){
+		p.play();
+	});
+}
+
+p.addEventListener("loadeddata", function() {
+	document.getElementById("load").style.display = "none";
+})
+
+document.addEventListener("keydown",function(event) {
+	if (event.defaultPrevented) {
+        return;
+    }
+	
+	var key = event.keyCode;
+	
+	if (key == 75 | key == 32) {
+		toggle();
+	} else if (key == 70) {
+		fullscreen();
+	}
+})
+
+function fullscreen() {
+	var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+		(document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+		(document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+		(document.msFullscreenElement && document.msFullscreenElement !== null);
+	if (!isInFullScreen) {
+		if (document.getElementById("player").requestFullscreen) {
+			document.getElementById("player").requestFullscreen();
+		} else if (document.getElementById("player").mozRequestFullScreen) {
+			document.getElementById("player").mozRequestFullScreen();
+		} else if (document.getElementById("player").webkitRequestFullScreen) {
+			document.getElementById("player").webkitRequestFullScreen();
+		} else if (document.getElementById("player").msRequestFullscreen) {
+			document.getElementById("player").msRequestFullscreen();
+		}
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.webkitExitFullscreen) {
+			document.webkitExitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.msExitFullscreen) {
+			document.msExitFullscreen();
+		}
+	}
+}
+
+function toggle() {
+	if (!document.getElementById("player").paused) {
+		document.getElementById("player").pause();
+	} else {
+		document.getElementById("player").play();
+	}
+}
